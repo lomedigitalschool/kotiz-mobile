e1827b0de11c6e0bdc3a85bddcb1de531cab9348
Romuald <147240143+Moruald001@users.noreply.github.com>
2025-08-19 18:01:08 +0000

    Feature/#145 kotiz app opening (#1)
    * splash screen set and onboarding done
    
    * native splash screen done
    
    * custom splash screen done
    
    * refacto  code ,  splash screen logo done splash screen animation done

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:kotiz_app/views/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final showHome = prefs.getBool("showHome") ?? false;
+
+  runApp(MyApp(showHome: showHome));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.showHome});
+  final bool showHome;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: "Roboto",
+        colorSchemeSeed: Color(0xffffffff),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: SplashScreen(showHome: showHome),
     );
   }
 }
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:kotiz_app/main.dart';
+import 'package:kotiz_app/views/home_page.dart';
+import 'package:kotiz_app/views/onboarding.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key, required this.showHome});
+
+  final bool showHome;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controllerAnimation;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _translateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerAnimation = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    // definition de l'effet de fade
+    _fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controllerAnimation,
+        curve: Curves.easeInOutSine,
+      ),
+    );
+
+    // definition de l' effet de rétrécissement
+    _scaleAnimation = Tween(begin: 4.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controllerAnimation, curve: Curves.easeInOut),
+    );
+
+    _translateAnimation =
+        Tween<Offset>(begin: Offset(0, 0), end: Offset(0, -2.05)).animate(
+          CurvedAnimation(
+            parent: _controllerAnimation,
+            curve: Interval(
+              0.8, // début du slide
+              1.0, // fin du slide
+              curve: Curves.easeInOut,
+            ),
+          ),
+        );
+
+    // lancement de l'animation
+    _controllerAnimation.forward();
+
+    Timer(Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => widget.showHome ? HomePage() : OnBoarding(),
+        ),
+      );
+    });
+  }
+
+  // liberation de la mémoire
+
+  @override
+  void dispose() {
+    _controllerAnimation.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: SlideTransition(
+              position: _translateAnimation,
+              child: Image.asset("assets/images/onBoardingLogo.png"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
